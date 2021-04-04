59474c4e4f33c3906be2069b3c8d2cca3d095f60
Ata-92 <atakal92@gmail.com>
2021-04-04 13:58:14 +0300

    Add for of solution

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ var topStudents = [
   {no: 345, name: "Bill", grade: 93}
 ]
 
-console.log(`${topStudents[0].name},${topStudents[1].name},${topStudents[2].name}`)
+// console.log(`${topStudents[0].name},${topStudents[1].name},${topStudents[2].name}`);
+
+
+let result = "";
+
+for (let obj of topStudents) {
+  result += obj.name + ",";
+}
+
+console.log(result.slice(0, -1));
