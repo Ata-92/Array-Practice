6f7fdbee278b261262ad5283dbac0c5208796149
Ata-92 <atakal92@gmail.com>
2021-04-04 13:47:02 +0300

    Add first solution

---
+++ app.js
@@ -0,0 +1,7 @@
+var topStudents = [
+  {no: 201, name: "Jane", grade: 98},
+  {no: 242, name: "Eric", grade: 95},
+  {no: 345, name: "Bill", grade: 93}
+]
+
+console.log(`${topStudents[0].name},${topStudents[1].name},${topStudents[2].name}`)
