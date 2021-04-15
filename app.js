e44e1f5c441bf4be18615623b7655c853eb02b61
Ata-92 <atakal92@gmail.com>
2021-04-15 16:06:28 +0300

    Add a function to check if a word is palindrome

--- app.js
+++ app.js
@@ -125,6 +125,13 @@
 
 
 
-const arr = [ [[1,2], [-1,-2]], ["a", "b"] ];
-const newArr = [arr[0][0][0], arr[0][0][1], arr[0][1][0], arr[0][1][1], arr[1][0], arr[1][1]];
-console.log(newArr);
+// const arr = [ [[1,2], [-1,-2]], ["a", "b"] ];
+// const newArr = [arr[0][0][0], arr[0][0][1], arr[0][1][0], arr[0][1][1], arr[1][0], arr[1][1]];
+// console.log(newArr);
+
+
+
+(function (word) {
+  let reversedWord = word.split("").reverse().join("");
+  word == reversedWord ? alert(`Your word ${word} is a palindrome`) : alert(`Your word ${word} is not a palindrome\nTry again`);
+})(prompt("Please enter a word : "));
