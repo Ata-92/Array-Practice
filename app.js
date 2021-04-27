c42d943d85a81b76f84ac40d1c6391ad64d3e131
Ata-92 <atakal92@gmail.com>
2021-04-27 03:13:46 +0300

    Add key-value pairs to Map

--- app.js
+++ app.js
@@ -236,6 +236,7 @@ for (let i = 1; i < 6; i++) {
   for (let j = 1; j < 6; j++) {
       objects[`Lesson-${j}`] = Math.floor(Math.random() * (100 - 35 + 1) + 35);
   }
-
+  scores.set(`Student-${i}`, objects);
 }
+
 
